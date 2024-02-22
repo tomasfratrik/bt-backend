529e849f5bd9ce044ee0032e17d8ab3ab529dc77
tomasfratrik <tomasfratrik8@gmail.com>
2024-02-22 10:36:11 +0100

    heroku: try uploaded img for grisa

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from grisa import Grisa
 
 app = Flask(__name__)
 CORS(app)
+IMG_DIR = "images"
 
 @app.route('/ping', methods=['GET'])
 def index():
@@ -57,42 +58,25 @@ def grisa():
 
         if file.filename == '':
             return jsonify({'error': 'No selected file'})
-        # print(f"filename: {file.filename}")
-        img_folder = "images"
-        #upload to img folder
-        img_id = str(uuid.uuid4())
-        img_name = f"{img_id}.png"
-        img_path = os.path.join(img_folder, img_name)
-        file.save(img_path)
 
+        # get image extension
+        extention = file.filename.split('.')[-1]
+        img_random_id = str(uuid.uuid4())
+        img_new_name = f"{img_random_id}.{extention}"
+        full_path = os.path.join(IMG_DIR, img_new_name)
+        file.save(full_path)
 
-
-        # PATH="./driver/chromedriver/chromedriver"
         grisa = Grisa()
-        # grisa.set_driver_path(PATH)
         grisa.set_driver_path("CHROMEDRIVER_PATH")
         grisa.set_binary_path("GOOGLE_CHROME_BIN")
         grisa.init_driver()
-        # relative_path = f"{img_folder}/{img_name}"
-        # full_path = os.path.join(os.getcwd(), relative_path)
-        relative_path = "house.jpeg"
-        full_path = os.path.join(os.getcwd(), relative_path)
-        grisa.run(full_path, accept_cookies=True)
-        # grisa.run(full_path)
-        # grisa.run("https://im9.cz/sk/iR/importprodukt-orig/808/808fd902a98bc47d11d06a91f2af9424--mm2000x2000.jpg")
-        # sleep(2)
+        grisa.run(full_path, accept_cookies=True, local=False)
+
         page_source = grisa.get_page_source()
         similiar_img_json = grisa.scrape_similiar(page_source)
-        # grisa.go_to_source()
-        # page_source = grisa.get_page_source()
-        # source_img_json = grisa.scrape_source(page_source)
-        # print(f"similiar_img_json: {similiar_img_json}")
-        # print(100*"*")
-        # print(f"source_img_json: {source_img_json}")
         grisa.driver_quit()
-        # return similiar_img_json, source_img_json
-        # remove file from img folder
-        os.remove(img_path)
+
+        os.remove(full_path)
         print(f"similiar_img_json: {similiar_img_json}")
         return jsonify(similiar_img_json)
 
