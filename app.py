7672d5b1265001b7f564d263c0b8c67027550192
tomasfratrik <tomasfratrik8@gmail.com>
2024-02-22 09:41:11 +0100

    Trying grisa_test for heroku

--- app.py
+++ app.py
@@ -18,6 +18,35 @@ CORS(app)
 def index():
     return jsonify('pong!')
 
+@app.route('/grisa_test', methods=['GET', 'POST'])
+def grisa_test():
+    # PATH="./driver/chromedriver/chromedriver"
+    grisa = Grisa()
+    grisa.set_driver_path(PATH)
+    grisa.set_driver_path("CHROMEDRIVER_PATH")
+    grisa.set_binary_path("GOOGLE_CHROME_BIN")
+    grisa.init_driver()
+    # relative_path = "audia6.png"
+    relative_path = "house.jpeg"
+    full_path = os.path.join(os.getcwd(), relative_path)
+    # grisa.run(full_path, accept_cookies=True, local=True)
+    grisa.run(full_path, accept_cookies=True)
+    # grisa.run("https://im9.cz/sk/iR/importprodukt-orig/808/808fd902a98bc47d11d06a91f2af9424--mm2000x2000.jpg")
+    # sleep(2)
+    page_source = grisa.get_page_source()
+    similiar_img_json = grisa.scrape_similiar(page_source)
+    # grisa.go_to_source()
+    # page_source = grisa.get_page_source()
+    # source_img_json = grisa.scrape_source(page_source)
+    # print(f"similiar_img_json: {similiar_img_json}")
+    # print(100*"*")
+    # print(f"source_img_json: {source_img_json}")
+    grisa.driver_quit()
+    # return similiar_img_json, source_img_json
+    # print(f"similiar_img_json: {similiar_img_json}")
+    return jsonify(similiar_img_json)
+
+
 @app.route('/grisa', methods=['GET', 'POST'])
 def grisa():
     if request.method == 'POST':
